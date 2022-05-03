73cf8e4e2245e33f73ba09d958ac8875950d1060
Lucas Coelho <58010418+LucasVinicius314@users.noreply.github.com>
2022-05-03 18:20:59 -0300

    js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log(1)
+const sdk = require('facebook-nodejs-business-sdk')
+
+const accessToken = process.env['ACCESS_TOKEN']
+
+const adAccountId = process.env['AD_ACCOUNT_ID']
+
+const api = sdk.FacebookAdsApi.init(accessToken)
+
+const account = new sdk.AdAccount(adAccountId)
+
+// api.
