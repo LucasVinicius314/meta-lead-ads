5ac45fbfac355e0b9d7aca0f2f7d6ff94ec0bf93
Lucas Coelho <58010418+LucasVinicius314@users.noreply.github.com>
2022-05-03 18:53:38 -0300

    fb sdk

--- src/main/java/com/sure/Env.java
+++ src/main/java/com/sure/Env.java
@@ -2,7 +2,11 @@ package com.sure;
 
 public class Env {
 
-    public static final String accessToken = System.getenv("ACCESS_TOKEN");
+  public static final String accessToken = System.getenv("ACCESS_TOKEN");
 
-    public static final String adAccountId = System.getenv("AD_ACCOUNT_ID");
+  public static final String adAccountId = System.getenv("AD_ACCOUNT_ID");
+
+  public static final String appId = System.getenv("APP_ID");
+
+  public static final String appSecret = System.getenv("APP_SECRET");
 }
--- src/main/java/com/sure/FB.java
+++ src/main/java/com/sure/FB.java
@@ -1,8 +1,23 @@
 package com.sure;
 
 import com.facebook.ads.sdk.APIContext;
+import com.facebook.ads.sdk.AdAccount;
 
 public class FB {
 
-    public static final APIContext context = new APIContext(Env.accessToken, Env.adAccountId);
+  public static final APIContext context = new APIContext(Env.accessToken, Env.appSecret);
+
+  public FB() {
+    final var account = new AdAccount(Env.adAccountId, context);
+
+    try {
+      final var campaigns = account.getCampaigns().requestAllFields().execute();
+
+      for (final var campaign : campaigns) {
+        System.out.println(campaign.getFieldName());
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
 }
